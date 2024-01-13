dda25259d680aa46ba70c96aa27b661c2d8bed26
Blessed Adesina <adesinablessed19@gmail.com>
2024-01-13 00:32:11 -0500

    changes

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 if __name__ = "__main__":
-    from sys import argv
-    if len(argv) == 1:
+    import sys
+    if len(sys.argv) == 1:
         print("{} arguments.".format(0))
-    elif len(argv) == 2:
+    elif len(sys.argv) == 2:
         print("{} argument:".format(1))
-        print("{}: {}".format(1, argv[1]))
-    elif len(argv) > 2:
-        print("{} arguments:".format(len(argv) - 1))
-        for i in range(1, len(argv)):
-            print("{}: {}".format(i, argv[i]))
+        print("{}: {}".format(1, sys.argv[1]))
+    elif len(sys.argv) > 2:
+        print("{} arguments:".format(len(sys.argv) - 1))
+        for i in range(1, len(sys.argv)):
+            print("{}: {}".format(i, sys.argv[i]))
