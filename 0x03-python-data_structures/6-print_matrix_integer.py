c8820c2ccd4f3f3a2416a62556f31c9039b50600
Blessed Adesina <adesinablessed19@gmail.com>
2024-01-17 18:01:06 -0500

    changes

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    if matrix:
+    if not matrix:
+        print()
+    else:
         for i in range(len(matrix)):
             for j in range(len(matrix[i])):
                 print("{:d}".format(matrix[i][j]), end=' '
