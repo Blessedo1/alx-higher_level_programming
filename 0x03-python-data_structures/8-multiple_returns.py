e4a651444b07a00c656b285a0383cd6124741972
Blessed Adesina <adesinablessed19@gmail.com>
2024-01-17 20:10:38 -0500

    changes

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    if not sentence:
+    if sentence == "":
         length = 0
         first = None
-    elif sentence:
+    else:
         length = len(sentence)
         first = sentence[0]
     return(length, first)
