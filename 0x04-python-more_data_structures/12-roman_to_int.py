25821ba8db53c286c8a93c3571acdcc690496d92
Blessed Adesina <adesinablessed19@gmail.com>
2024-02-03 17:18:38 -0500

    changes

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    standard = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    if not isinstance(roman_string, str) or roman_string is None:
+        return 0
+    standrd = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     total = 0
     prev_value = 0
     roman = list(roman_string)
     for i in reversed(roman):
-        value = standard[i]
+        value = standrd[i]
         if value < prev_value:
             total -= value
         else:
