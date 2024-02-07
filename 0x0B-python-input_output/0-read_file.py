a04017284e23dcc65372406e391b0a722360b9e5
Blessed Adesina <adesinablessed19@gmail.com>
2024-02-06 20:23:44 -0500

    code updated

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
+"""Defines a text file-reading function"""
+
 def read_file(filename=""):
     """reads a text file in UTF8 and prints to stdout"""
     with open(filename, mode='r', encoding='UTF8') as f:
