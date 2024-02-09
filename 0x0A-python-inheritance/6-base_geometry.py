51cd3eadb6f747a7aec8a80433934ebc2792b37d
Blessed Adesina <adesinablessed19@gmail.com>
2024-02-08 23:16:21 -0500

    initial code

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Create a BaseGeometry Module"""
+
+
+class BaseGeometry:
+    """Define a class BaseGeometry"""
+
+    def area(self):
+        raise Exception("area() is not implemented")
