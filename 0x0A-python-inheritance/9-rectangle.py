fe4a3bf585899902288c37e2786d2fea811540b2
Blessed Adesina <adesinablessed19@gmail.com>
2024-02-09 15:59:56 -0500

    initial code

---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Create a BaseGeometry class that is inherited by class Rectangle"""
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """Define a class Rectangle"""
+
+    def __init__(self, width, height):
+        """Instantiate with default attributes for Rectangle class"""
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
+    
+    def __str__(self):
+        return "[Rectangle] {}/{}".format(self.__width, self.__height)
+
+    def area(self):
+        return self.__width * self.__height
