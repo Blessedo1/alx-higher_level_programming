e1d597f3c994d8d5e01e7f3bf81768229c95d0fe
Blessed Adesina <adesinablessed19@gmail.com>
2024-02-08 21:24:06 -0500

    initial code

---
+++ 0x0A-python-inheritance/tests/2-is_same_class.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Defines a function that checks if an object is an instance of a class"""
+
+
+def is_same_class(obj, a_class):
+    """returns true if obj is an instance of a_class else false"""
+    if isinstance(obj, a_class):
+        return True
+    else:
+        return False
