70e00e71b6f7bbfbc7ce81670ab4a131e3580bd1
Blessed Adesina <adesinablessed19@gmail.com>
2024-02-09 17:27:08 -0500

    initial code

---
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def add_attribute(obj, name, value):
+    """adds a new attribute to the object if possible"""
+    if hasattr(obj, '__dict__'):
+        setattr(obj, name, value)
+    else:
+        raise TypeError("can't add new attribute")
