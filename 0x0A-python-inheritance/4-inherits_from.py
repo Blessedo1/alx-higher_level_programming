1c4f4b8679e01f349169d5d1941c66ec6e71f967
Blessed Adesina <adesinablessed19@gmail.com>
2024-02-08 23:04:47 -0500

    code changes

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -5,7 +5,7 @@
 def inherits_from(obj, a_class):
     """return True if an object is an instance of a subclass else False"""
     if issubclass(type(obj), a_class) is True:
-        if isinstance(obj, a_class) is True:
+        if type(obj) is not a_class:
             return True
     else:
         return False
