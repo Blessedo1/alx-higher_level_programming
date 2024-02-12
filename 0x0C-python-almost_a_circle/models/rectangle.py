4168a71786309a6219f610e18a5d90e02baaddd9
Blessed Adesina <adesinablessed19@gmail.com>
2024-02-12 18:26:49 -0500

    changes

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -4,7 +4,7 @@ from models.base import Base
 
 
 class Rectangle(Base):
-    """Define a class Rectangle with getter and setter methods"""
+    """Define a class Rectangle with methods"""
 
     @staticmethod
     def int_validation(name, value):
@@ -25,7 +25,20 @@ class Rectangle(Base):
             raise ValueError("{} must be >= 0".format(name))
 
     def __init__(self, width, height, x=0, y=0, id=None):
-        """Instantiate the Rectangle class object"""
+        """Initialize the Rectangle class object
+        
+        Args:
+            width (int): The width of the new Rectangle.
+            height (int): The height of the new Rectangle.
+            x (int): The x coordinate of the new Rectangle.
+            y (int): The y coordinate of the new Rectangle.
+            id (int): The identity of the new Rectangle.
+        Raises:
+            TypeError: If either of width or height is not an int.
+            ValueError: If either of width or height <= 0.
+            TypeError: If either of x or y is not an int.
+            ValueError: If either of x or y < 0.
+        """
         self.int_validation("width", width)
         self.positive_validation('width', width)
         self.int_validation("height", height)
