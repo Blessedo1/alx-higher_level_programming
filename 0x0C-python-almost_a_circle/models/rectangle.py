e1229a3c3fb56b1f00916c3ae06db2a6867ecd9b
Blessed Adesina <adesinablessed19@gmail.com>
2024-02-12 17:45:14 -0500

    initial code

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -6,12 +6,36 @@ from models.base import Base
 class Rectangle(Base):
     """Define a class Rectangle with getter and setter methods"""
 
+    @staticmethod
+    def int_validation(name, value):
+        if type(value) is not int:
+            raise TypeError("{} must be an integer".format(name))
+
+    @staticmethod
+    def positive_validation(name, value):
+        if value <= 0:
+            raise ValueError("{} must be > 0".format(name))
+
+    @staticmethod
+    def positive_coord_validation(name, value):
+        if value < 0:
+            raise ValueError("{} must be >= 0".format(name))
+
     def __init__(self, width, height, x=0, y=0, id=None):
         """Instantiate the Rectangle class object"""
-        self.__width = width
-        self.__height = height
-        self.__x = x
-        self.__y = y
+        self.int_validation("width", width)
+        self.positive_validation('width', width)
+        self.int_validation("height", height)
+        self.positive_validation('height', height)
+        self.int_validation('x', x)
+        self.positive_coord_validation('x', x)
+        self.int_validation('y', y)
+        self.positive_coord_validation('y', y)
+
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
         super().__init__(id)
 
     @property
@@ -37,19 +61,78 @@ class Rectangle(Base):
     @tweak_width.setter
     def tweak_width(self, value):
         """set the width of the rectangle"""
+        if type(value) != int:
+            raise TypeError("width must be an integer")
+        if value <= 0:
+            raise ValueError("width must be > 0")
         self.__width = value
 
     @tweak_height.setter
     def tweak_height(self, value):
         """set the height of the rectangle"""
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        if value <= 0:
+            raise ValueError("height must be > 0")
         self.__height = value
 
     @tweak_x.setter
     def tweak_x(self, value):
         """set the x coordinates of the rectangle"""
+        if type(sel.__x) != int:
+            raise TypeError("x must be an integer")
+        if value < 0:
+            raise ValueError("x must be >= 0")
         self.__x = value
 
     @tweak_y.setter
     def tweak_y(self, value):
         """set the y coordinates of the rectangle"""
+        if type(value) is not int:
+            raise TypeError("y must be an integer")
+        if value < 0:
+            raise ValueError("y must be >= 0")
         self.__y = value
+
+    def area(self):
+        return self.width * self.height
+
+    def display(self):
+        for y in range(self.y):
+            print()
+        for h in range(self.height):
+            for x in range(self.x):
+                print(" ", end='')
+            for w in range(self.width):
+                print("#", end='')
+            print()
+
+    def update(self, *args, **kwargs):
+        if args and len(args) != 0:
+            if len(args) >= 1:
+                self.id = args[0]
+            if len(args) >= 2:
+                self.width = args[1]
+            if len(args) >= 3:
+                self.height = args[2]
+            if len(args) >= 4:
+                self.x = args[3]
+            if len(args) >= 5:
+                self.y = args[4]
+
+        elif kwargs and len(kwargs) != 0:
+            for i, j in kwargs.items():
+                if i == 'width':
+                    self.width = j
+                if i == 'height':
+                    self.height = j
+                if i == 'x':
+                    self.x = j
+                if i == 'y':
+                    self.y = j
+                if i == 'id':
+                    self.id = j
+
+    def __str__(self):
+        return f"[Rectangle] ({self.id})\
+ {self.x}/{self.y} - {self.width}/{self.height}"
---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python3
+"""Create a class square"""
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """Define a class square that inherits from rectangle"""
+
+    def __init__(self, size, x=0, y=0, id=None):
+        """Initialization of the class instance"""
+        super().__init__(width, height, x, y, id)
+
+    def __str__(self):
+        """Return the print() and str() representation of a square"""
+        return f"[Square] ({self.id}) {self.x}/{self.y} - {self.width}"
+
+    @property
+    def size(self):
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        if type(value) != int:
+            raise TypeError("width must be an integer")
+        if value <= 0:
+            raise ValueError("width must be > 0")
+        self.width = value
+        self.height = value
+
+    def update(self, *args, **kwargs):
+        if args and len(args) != 0:
+            if len(args) >= 1:
+                self.size = args[0]
+            elif len(args) >= 2:
+                self.size = args[1]
+            elif len(args) >= 3:
+                self.size = args[2]
+            elif len(args) >= 4:
+                self.size = args[3]
+
+        elif kwargs and len(kwargs) != 0:
+            for i, j in kwargs.items():
+                if i == 'size':
+                    self.width = j
+                elif i == 'x':
+                    self.x = j
+                elif i == 'y':
+                    self.y = j
+                elif i == 'id':
+                    self.id = j
