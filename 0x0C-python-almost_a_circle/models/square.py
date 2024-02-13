115ecc6dfe17438b7b2a273f2ed5c7996a4e4a22
Blessed Adesina <adesinablessed19@gmail.com>
2024-02-12 19:44:12 -0500

    changes

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -8,6 +8,8 @@ class Square(Rectangle):
 
     def __init__(self, size, x=0, y=0, id=None):
         """Initialization of the class instance"""
+        width = size
+        height = size
         super().__init__(width, height, x, y, id)
 
     def __str__(self):
