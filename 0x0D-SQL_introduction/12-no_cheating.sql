2ee717a9a9fb571898cbf8705df49630f7f29bba
Blessed Adesina <adesinablessed19@gmail.com>
2024-06-28 14:43:56 +0100

    solution updated

--- 0x0D-SQL_introduction/12-no_cheating.sql
+++ 0x0D-SQL_introduction/12-no_cheating.sql
@@ -2,4 +2,4 @@
 -- not allowed to use Bob's id value, only the name field
 -- The database name will be passed as an argument of the mysql command
 
-UPDATE second_table SET score = 10 WHERE name = Bob
+UPDATE second_table SET score = 10 WHERE name = 'Bob'
