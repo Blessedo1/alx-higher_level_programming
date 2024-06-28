61347ce6002358a27d071bdd53e356155340c536
Blessed Adesina <adesinablessed19@gmail.com>
2024-06-28 17:18:20 +0100

    solutions updated

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,4 +1,4 @@
 -- prints the full description of the table 'first_table' from the database in MySQL server
 -- database name will be passed as an argument of the mysql command
 
-SHOW CREATE TABLE first_table
+SHOW CREATE TABLE first_table;
