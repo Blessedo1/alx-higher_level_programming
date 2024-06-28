53ed54238a02c5bd369f136bdcf0441f1ba0d3b3
Blessed Adesina <adesinablessed19@gmail.com>
2024-06-28 13:07:36 +0100

    solutions updated

---
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -0,0 +1,8 @@
+-- creates a taable 'second_table' in the database'hbtn_0c_0' in MySQL server and add multiple rows
+-- the database will be passed as an argument to the mysql command
+
+CREATE TABLE (id INT,
+	name VARCHAR(256),
+	score INT
+) IF NOT EXISTS;
+INSERT INTO second_table (id, name, score) VALUES (1, 'John', 10), (2, 'Alex', 3), (3, 'Bob', 14), (4, 'George', 8)
