4a8e7f2d144feeb1acb3c96536deba6d053fd66c
Blessed Adesina <adesinablessed19@gmail.com>
2024-06-28 13:10:37 +0100

    solutions updated

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -4,5 +4,5 @@
 CREATE TABLE (id INT,
 	name VARCHAR(256),
 	score INT
-) IF NOT EXISTS;
-INSERT INTO second_table (id, name, score) VALUES (1, 'John', 10), (2, 'Alex', 3), (3, 'Bob', 14), (4, 'George', 8)
+) IF NOT EXISTS second_table;
+INSERT INTO second_table (id, name, score) VALUES (1, 'John', 10), (2, 'Alex', 3), (3, 'Bob', 14), (4, 'George', 8);
