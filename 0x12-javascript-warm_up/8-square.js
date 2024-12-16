3d44ca063425a6232c22a9e2f78e7adb67d70055
Blessed Adesina <adesinablessed19@gmail.com>
2024-12-17 00:51:42 +0100

    code changes

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+const size = process.argv;
+
+if (!isNaN(size[2])) {
+  for (let i = 0; i < size[2]; i++) {
+    let row = '';
+    for (let j = 0; j < size[2]; j++) {
+      row += 'X';
+    }
+    console.log(row);
+  }
+} else if (isNaN(size[2])) {
+  console.log('Missing size');
+}
